065950c1d283b6b14d920bba69e039af9a173a7b
DevSecOpsVince <32544618+vincentjking@users.noreply.github.com>
2022-06-15 11:06:22 +0100

    Create LIV-Config.ps1

---
+++ Config/LIV-Config.ps1
@@ -0,0 +1,8 @@
+# LIV config file
+$Global:config = @{
+    cveLogFolder = '' # Folder location for plain text log files
+    cveOutputFolder = '' # Folder location for output file
+    smtpServer = '' # SMTP server address
+    cveEmailFrom = '' # From address
+    cveEmailTo = '' # To addresses (must be , separated)
+}   
